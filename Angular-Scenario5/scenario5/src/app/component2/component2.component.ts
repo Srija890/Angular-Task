a0335fa90923dccecf0c6cbc6cb32f7c702d0f9e
srija <srijak890@gmail.com>
2019-08-07 22:18:14 +0530

    AngularTask

---
+++ Angular-Scenario5/scenario5/src/app/component2/component2.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit,Output,EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-component2',
+  templateUrl: './component2.component.html',
+  styleUrls: ['./component2.component.css']
+})
+export class Component2Component implements OnInit {
+  public greeting;
+  // Event is created
+  @Output() public childEvent=new EventEmitter();
+     constructor() { }
+  
+    ngOnInit() {
+    }
+    // Emits the message from the child component to other component when called 
+  fireEvent()
+  {
+    this.childEvent.emit(this.greeting);
+  }
+  }
+  
